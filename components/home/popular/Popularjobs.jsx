fff4c8a92b0e747ed04d09df67cb722c2f15a366
blackweeed <blackweeeed@gmail.com>
2023-03-21 10:16:40 +0100

    popularjob component finished

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -20,8 +20,6 @@ const Popularjobs = () => {
     num_pages: 1,
   });
 
-  console.log(data);
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -38,7 +36,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
